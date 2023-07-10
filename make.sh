867a093ce0cf78c00fd56ddda80275225d1079d2
Nicholas Obert <nchlsuba@gmail.com>
2023-07-10 12:03:25 +0200

    updated deprecated install options

--- make.sh
+++ make.sh
@@ -1,3 +1,11 @@
 #/bin/bash
-python3 setup.py build
-sudo python3 setup.py install
+
+# Old version, deprecated
+#python3 setup.py build
+#sudo python3 setup.py install
+
+# New version
+python3 -m venv .venv
+source .venv/bin/activate
+python3 -m build
+pip install .
